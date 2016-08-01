47d895fa4a43bbe8b1ba8b3d467bcfb67f5e6cd0
huayangyu <lovehuayangyu@163.com>
2016-08-01 09:36:26 +0800

    Update ZYBannerCell.m
    自定义view写里面

--- ZYBannerView/ZYBannerCell.m
+++ ZYBannerView/ZYBannerCell.m
@@ -1,5 +1,6 @@
 //
 //  ZYBannerCell.m
+//  DuoBao
 //
 //  Created by 张志延 on 15/10/17.
 //  Copyright (c) 2015年 tongbu. All rights reserved.
@@ -9,4 +10,19 @@
 
 @implementation ZYBannerCell
 
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self.contentView addSubview:self.imageView];
+    }
+    return self;
+}
+
+#pragma mark - Getters
+- (UIImageView *)imageView {
+    if (!_imageView) {
+        _imageView = [[UIImageView alloc] initWithFrame:self.bounds];
+        _imageView.contentMode = UIViewContentModeScaleAspectFill;
+    }
+    return _imageView;
+}
 @end
